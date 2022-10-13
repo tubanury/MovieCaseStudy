334de2b246d106547dc42a02290e98b8a5d9f992
tuba <yldztubanur12@gmail.com>
2022-10-13 20:58:17 +0300

    Splash screen redesign

---
+++ MovieCaseStudy/Utilities/UIFont.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont.swift
+//  MovieCaseStudy
+//
+//  Created by Tuba N. Yıldız on 13.10.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static let pixel17 = UIFont(name: "Starjedi", size: 17.0)
+    
+}
