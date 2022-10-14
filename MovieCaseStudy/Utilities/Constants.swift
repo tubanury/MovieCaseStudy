d87043cd3695fc1b972ec71aadc21c0b2c2d1450
tuba <yldztubanur12@gmail.com>
2022-10-14 17:32:13 +0300

    Refactor Code

--- MovieCaseStudy/Utilities/Constants.swift
+++ MovieCaseStudy/Utilities/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum Api {
     static let baseUrl = "http://www.omdbapi.com/"
-    static let apikey = "8c78aad9"
+    static let apikey = ""
     static let parameterForSearch = "?s="
     static let parameterForImdb = "?i="
     static let parameterForApikey = "&apikey="
